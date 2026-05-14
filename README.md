2dd11a748cdb75b6324d1ff940e74fab97e8531a
SorinaPopa <sorinapopaan@gmail.com>
2026-05-14 23:24:33 +0300

    added light dimmer on the touch sensor of the hw device

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dissertation2026
+
+The code for the mobile app and the hardware device is taken and modified from the bachelor thesis project: https://github.com/SorinaPopa/MoodUp.git
--- hardware/moodup_hw/moodup_hw.ino
+++ hardware/moodup_hw/moodup_hw.ino
@@ -13,10 +13,6 @@
 //include passwords and keys
 #include "secrets.h"
 
-// provide the token generation process info and the RTDB payload printing info and other helper functions
-// #include "addons/TokenHelper.h"
-// #include "addons/RTDBHelper.h"
-
 // define sensors pin numbers
 #define TOUCH_PIN 14
 #define DHT_PIN 5
@@ -36,27 +32,28 @@ FirebaseConfig config;
 
 //variables initialisation 67
 unsigned long sendDataPrevMillis = 0;
-unsigned long lastMotionRead = 0;
 float floatTemp = 0;
 float floatHumid = 0;
 int intLight = 0;
-int intTouch = 0;
 int intRed = 0;
 int intGreen = 0;
 int intBlue = 0;
-int motionDetected = 0;
 bool signupOK = false;
 
 // dht, light sensor, led ring initialisation
-DHT dht(DHT_PIN,DHTTYPE);
+DHT dht(DHT_PIN, DHTTYPE);
 BH1750 lightMeter;
 CRGB leds[NUM_LEDS];
 
-void wifiConnection(){
+// dimmer - SIX SEVEN
+int brightnessLevel = 0;
+const int brightnessValues[] = { 255, 180, 120, 70, 30, 0 };
+bool previousTouchState = false;
+
+void wifiConnection() {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(300);
   }
@@ -66,29 +63,21 @@ void wifiConnection(){
   Serial.println();
 }
 
-void firebaseSignUp(){
-  // assign the api key and the RTDB URL 
+void firebaseSignUp() {
+  // assign the api key and the RTDB URL
   config.api_key = API_KEY;
   config.database_url = DATABASE_URL;
 
   // anonymous sign up
-  if (Firebase.signUp(&config, &auth, "", ""))
-  {
+  if (Firebase.signUp(&config, &auth, "", "")) {
     Serial.println("ok");
     signupOK = true;
-  }
-  else
-  {
+  } else {
     Serial.printf("%s\n", config.signer.signupError.message.c_str());
   }
-
-  // assign the callback function for the long running token generation task 
-  //config.token_status_callback = tokenStatusCallback; 
-
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
 }
-
 
 void setup() {
   Serial.begin(115200);
@@ -100,41 +89,40 @@ void setup() {
   //dht sensor setup
   dht.begin();
   //light sensor setup
-  Wire.begin(SDA,SCL);
-  if(lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE, LIGHT_SENSOR_ADDR))
-  {
+  Wire.begin(SDA, SCL);
+  if (lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE, LIGHT_SENSOR_ADDR)) {
     Serial.println("BH1750 started");
-  }
-  else
-  {
+  } else {
     Serial.println("Error initialising BH1750");
   }
   //led ring setup
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(80);
+  FastLED.setBrightness(brightnessValues[brightnessLevel]);
 }
 
-void readTouchSensor(){
-    int touch = digitalRead(TOUCH_PIN);
-  if (touch == HIGH)
-  {
-    intTouch = 0;
-    Serial.println("Touch touched!");
-  }
-  else
-  {
-    intTouch = 1;
-    Serial.println("Touch not touched!");
-  }
+void handleTouchSensor() {
+  bool currentTouchState = (digitalRead(TOUCH_PIN) == LOW);
+  if (currentTouchState && !previousTouchState) {
+    brightnessLevel++;
+    if (brightnessLevel > 5) {
+      brightnessLevel = 0;
+    }
+    if (brightnessLevel == 0) {
+      readLightSensor();
+    }
+    FastLED.setBrightness(brightnessValues[brightnessLevel]);
+    FastLED.show();
 
+    delay(250);
+  }
+  previousTouchState = currentTouchState;
 }
 
-void readDHTSensor(){
+void readDHTSensor() {
   floatTemp = dht.readTemperature();
   floatHumid = dht.readHumidity();
 
-  if (isnan(floatTemp) || isnan(floatHumid))
-  {
+  if (isnan(floatTemp) || isnan(floatHumid)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
@@ -146,115 +134,78 @@ void readDHTSensor(){
   Serial.println(" %");
 }
 
-void readLightSensor(){
-  lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE, LIGHT_SENSOR_ADDR);
+void readLightSensor() {
   intLight = lightMeter.readLightLevel();
   Serial.print("Light Level: ");
   Serial.print(intLight);
   Serial.println(" lux");
 }
 
-void sendSensorData(){
+void sendSensorData() {
   // write temperature data
-  if (Firebase.RTDB.setFloat(&fbdo, "devices/esp32/sensors/temperature", floatTemp))
-  {
+  if (Firebase.RTDB.setFloat(&fbdo, "devices/esp32/sensors/temperature", floatTemp)) {
     Serial.println("PASSED");
     Serial.println("PATH: " + fbdo.dataPath());
     Serial.println("TYPE: " + fbdo.dataType());
-  }
-  else
-  {
+  } else {
     Serial.println("FAILED");
     Serial.println("REASON: " + fbdo.errorReason());
   }
 
   // write humidity data
-  if (Firebase.RTDB.setFloat(&fbdo, "devices/esp32/sensors/humidity", floatHumid))
-  {
+  if (Firebase.RTDB.setFloat(&fbdo, "devices/esp32/sensors/humidity", floatHumid)) {
     Serial.println("PASSED");
     Serial.println("PATH: " + fbdo.dataPath());
     Serial.println("TYPE: " + fbdo.dataType());
-  }
-  else
-  {
+  } else {
     Serial.println("FAILED");
     Serial.println("REASON: " + fbdo.errorReason());
   }
 
   // write light data
-  if (Firebase.RTDB.setInt(&fbdo, "devices/esp32/sensors/light", intLight))
-  {
+  if (Firebase.RTDB.setInt(&fbdo, "devices/esp32/sensors/light", intLight)) {
     Serial.println("PASSED");
     Serial.println("PATH: " + fbdo.dataPath());
     Serial.println("TYPE: " + fbdo.dataType());
-  }
-  else
-  {
+  } else {
     Serial.println("FAILED");
     Serial.println("REASON: " + fbdo.errorReason());
   }
 }
 
-void sendTouchSensorData(){
-  // write touch sensor data
-  if (Firebase.RTDB.setInt(&fbdo, "devices/esp32/sensors/touch", intTouch))
-  {
-    Serial.println("PASSED");
-    Serial.println("PATH: " + fbdo.dataPath());
-    Serial.println("TYPE: " + fbdo.dataType());
-  }
-  else
-  {
-    Serial.println("FAILED");
-    Serial.println("REASON: " + fbdo.errorReason());
-  }
-}
-
-void readDbColourData(){
+void readDbColourData() {
   // read colour data
-  if (Firebase.RTDB.getInt(&fbdo, "/devices/esp32/colour/R"))
-  {
-    if (fbdo.dataType() == "int")
-    {
+  if (Firebase.RTDB.getInt(&fbdo, "/devices/esp32/colour/R")) {
+    if (fbdo.dataType() == "int") {
       intRed = fbdo.intData();
       Serial.println(intRed);
     }
-  }
-  else
-  {
+  } else {
     Serial.println(fbdo.errorReason());
   }
 
-  if (Firebase.RTDB.getInt(&fbdo, "/devices/esp32/colour/G"))
-  {
-    if (fbdo.dataType() == "int")
-    {
+  if (Firebase.RTDB.getInt(&fbdo, "/devices/esp32/colour/G")) {
+    if (fbdo.dataType() == "int") {
       intGreen = fbdo.intData();
       Serial.println(intGreen);
     }
-  }
-  else
-  {
+  } else {
     Serial.println(fbdo.errorReason());
   }
 
-  if (Firebase.RTDB.getInt(&fbdo, "/devices/esp32/colour/B"))
-  {
-    if (fbdo.dataType() == "int")
-    {
+  if (Firebase.RTDB.getInt(&fbdo, "/devices/esp32/colour/B")) {
+    if (fbdo.dataType() == "int") {
       intBlue = fbdo.intData();
       Serial.println(intBlue);
     }
-  }
-  else
-  {
+  } else {
     Serial.println(fbdo.errorReason());
   }
 }
 
-void setColourToLED(){
+void setColourToLED() {
   delay(100);
-   for(int i = 0; i < NUM_LEDS; i++){
+  for (int i = 0; i < NUM_LEDS; i++) {
     leds[i] = CRGB(intRed, intGreen, intBlue);
   }
   FastLED.show();
@@ -268,19 +219,21 @@ void setColourToLED(){
 }
 
 void loop() {
+  handleTouchSensor();
   if (Firebase.ready() && signupOK) {
 
     if (millis() - sendDataPrevMillis > 5000 || sendDataPrevMillis == 0) {
       sendDataPrevMillis = millis();
 
       //read sensors
-      readTouchSensor();
       readDHTSensor();
-      readLightSensor();
+
+      if (brightnessValues[brightnessLevel] == 0) {
+        readLightSensor();
+      }
 
       //send to Firebase
       sendSensorData();
-      sendTouchSensorData();
 
       //read colour from Firebase
       readDbColourData();
